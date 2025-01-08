b2456f1dc042ae2759c328ba7a3442c0b24ef6ca
tu pham <panhtu0902@gmail.com>
2025-01-08 23:42:30 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+// Middleware configuration
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
