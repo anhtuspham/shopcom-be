526abbbaaebc0e62245cb9e17c78fb87f607ab16
tu pham <panhtu0902@gmail.com>
2025-01-13 22:25:42 +0700

    feat(auth): update auth

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 
-export const connectDB = async() => {
+const connectDB = async() => {
+    
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
         })
-        log.info('MongoDB Connected...');
+        console.log('MongoDB Connected...');
     } catch(error){
         console.error(error.message);
         process.exit(1);
     }
 };
 
-module.exports = connectDB;
+export default connectDB;
