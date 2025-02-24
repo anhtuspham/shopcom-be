32ac9da75a4aa2565c50939065bbdd06eb9580db
tu pham <panhtu0902@gmail.com>
2025-02-24 22:13:10 +0700

    feat: Config json

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 // Middleware configuration
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use("/api/users", userRouter);
 app.use("/api/product", productRouter);
