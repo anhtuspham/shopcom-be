28dfeac9bee71e1d7dbb998a3997a27cc1019cc2
tu pham <panhtu0902@gmail.com>
2025-04-22 23:25:46 +0700

    feat: add api get orderById

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -113,6 +113,24 @@ const getUserOrders = asyncHandler(async (req, res) => {
   res.status(200).json(orders);
 });
 
+const getOrderById = asyncHandler(async (req, res) => {
+  const { orderId } = req.params;
+  const userId = req.user._id;
+
+  // const order = await Order.findById(orderId).populate("userId", "name email");
+  const order = await Order.findById(orderId);
+
+  if (!order) {
+    return res.status(404).json({ message: "Không tìm thấy đơn hàng" });
+  }
+
+  if (order.userId.toString() !== userId.toString()) {
+    return res.status(403).json({ message: "Không có quyền truy cập đơn hàng này" });
+  }
+
+  res.status(200).json(order);
+})
+
 const cancelOrder = asyncHandler(async (req, res) => {
   const { orderId } = req.params;
   const userId = req.user._id;
@@ -161,4 +179,5 @@ export {
   getUserOrders,
   cancelOrder,
   createPaymentIntent,
+  getOrderById
 };
--- src/routes/user/order-routes.js
+++ src/routes/user/order-routes.js
@@ -9,6 +9,7 @@ import {
   getUserOrders,
   cancelOrder,
   createPaymentIntent,
+  getOrderById
 } from "../../controllers/order-controller.js";
 
 const router = express.Router();
@@ -24,5 +25,6 @@ router.post("/:orderId/pay", protect, confirmPayment);
 router.post("/create-payment-intent", protect, createPaymentIntent);
 router.delete("/:orderId/cancel", protect, cancelOrder);
 router.get("/my-orders", protect, getUserOrders);
+router.get("/get-order/:orderId", protect, getOrderById);
 
 export default router;
